b719211730cf536956b6ab3c002ddbafaa332812
kiplangatwayne <waynekiplangatkorir903@gmail.com>
2023-06-19 13:41:22 +0300

    completed Asssingment

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
 // code your solution here
+function  superbowlWin(){
+    const record = [
+        { year: "2015", result: "W"},
+        { year: "2014", result: "N/A"},
+        { year: "2013", result: "L"},
+        //...
+      ]
+}
+function superbowlWin(records) {
+    const winRecord = records.find((record) => record.result === "W");
+  
+    if (winRecord) {
+      return winRecord.year;
+    } else {
+      return undefined;
+    }
+  }
+  
